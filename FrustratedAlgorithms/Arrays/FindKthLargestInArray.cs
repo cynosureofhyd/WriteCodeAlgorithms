9f99e67abe233658fbcdf0899b67e6ffcd82b697
cynosureofhyd <tomu644@gmail.com>
2014-04-09 23:39:44 -0700

    First Commit

---
+++ FrustratedAlgorithms/Arrays/FindKthLargestInArray.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrustratedAlgorithms.Arrays
+{
+    public class FindKthLargestInArray
+    {
+        public static int Find(List<int> array, int k)
+        {
+            if (array.Count() == 0)
+                return -100;
+            else
+            {
+                Random rand = new Random();
+                int pivot = rand.Next(0, array.Count() - 1);
+                return FindKth(array, 0, array.Count() - 1, pivot, k);
+            }
+        }
+
+        private static int FindKth(List<int> array, int start, int end, int pivot, int k)
+        {
+            int left = start;
+            int right = end;
+            while(left <= right)
+            {
+                while (left <= right && array[left] <= array[pivot])
+                    ++left;
+                while (left <= right && array[right] >= array[pivot])
+                    --right;
+                if (left <= right)
+                    swap(array, left, right);
+            }
+
+            return -1;
+        }
+
+        private static void swap(List<int> array, int left, int right)
+        {
+            if (array.Count() <= 0)
+                return;
+            else
+            {
+                int temp = array[left];
+                array[left] = array[right];
+                array[right] = temp;
+            }
+        }
+    }
+}
---
+++ FrustratedAlgorithms/MyRandom.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrustratedAlgorithms
+{
+    public class MyRandom
+    {
+        public static List<int> Random(int size)
+        {
+            List<int> result = new List<int>();
+            Random rand = new Random();
+            for(int i= 0; i < size; i++)
+            {
+                result.Add(rand.Next());
+            }
+            return result;
+        }
+
+        public static List<int> Random(int size, int maxValue)
+        {
+            List<int> result = new List<int>();
+            Random rand = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                result.Add(rand.Next(maxValue));
+            }
+            return result;
+        }
+    }
+}
